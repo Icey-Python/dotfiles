f7ffcbc2dd6a923998c24050ae0196379ddabf19
Kelly <kelly@dev.kelly>
2024-10-29 17:30:38 +0300

    refactor(lsp): Remove black formatter

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -95,7 +95,6 @@ null_ls.setup({
   debug = false,
   sources = {
     formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.black.with({ extra_args = { "--fast" } }),
     formatting.stylua,
     diagnostics.flake8
   },
